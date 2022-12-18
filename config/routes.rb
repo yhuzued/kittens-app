51f7e5841074dce13c3b5456b651111eeda73f12
yhuzued <yusufmait@gmail.com>
2022-12-18 08:31:13 +0700

    Create view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'kittens/index'
+  get 'kittens/show'
+  get 'kittens/new'
+  get 'kittens/edit'
+  get 'kittens/create'
+  get 'kittens/update'
+  get 'kittens/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
