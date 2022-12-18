40d6262f1d56421ad7347262076d17156fc82e63
yhuzued <yusufmait@gmail.com>
2022-12-18 09:24:58 +0700

    Implement html and json api

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,59 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @kittens}
+    end
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def create
+    @kitten = Kitten.new(app_params)
+    
+   if @kitten.save
+    redirect_to @kitten, notice: "Cat succesfully added!"
+   else
+    render :new, status: :unprocessable_entity
+   end
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(app_params)
+      redirect_to @kitten, notice: "Cat succesfully edited!"
+     else
+      render :edit, status: :unprocessable_entity
+     end
   end
 
   def destroy
+    @kitten = @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_path, notice: "Data is succesfully deleted."
+  end
+
+  private
+
+  def app_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/show'
-  get 'kittens/new'
-  get 'kittens/edit'
-  get 'kittens/create'
-  get 'kittens/update'
-  get 'kittens/destroy'
+  resources :kittens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "kittens#index"
 end
